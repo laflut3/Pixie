29eac793323f869e90cf99449a43b3c589368c4d
leo.torres <leo.torres@etu.umontpellier.fr>
2026-03-30 17:01:24 +0200

    feat: routage on site

--- pixie/src/main.rs
+++ pixie/src/main.rs
@@ -1,6 +1,9 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream},
+    path::Path,
+
 };
 
 fn main() {
@@ -13,13 +16,34 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let status_line = "HTTP/1.1 200 OK";
 
-    println!("Request: {http_request:#?}");
+    let file_name = request_line
+            .split_whitespace()
+            .nth(1)
+            .unwrap_or("")
+            .trim_start_matches('/')
+            .split('?')
+            .next()
+            .unwrap_or("");
+
+    let html_path = if file_name.is_empty() {
+        Path::new(env!("CARGO_MANIFEST_DIR")).join("../web/hello.html")
+    } else {
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("../web")
+            .join(format!("{file_name}.html"))
+    };
+
+    let contents = fs::read_to_string(html_path).unwrap();
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        stream.write_all(response.as_bytes()).unwrap();
 }
