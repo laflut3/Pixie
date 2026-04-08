eab1e8b0441455681e6165fe886c64e7f8e14fdf
leo.torres <leo.torres@etu.umontpellier.fr>
2026-04-08 11:16:07 +0200

    feat: lib threadpool

---
+++ pixie/src/lib.rs
@@ -0,0 +1,32 @@
+pub struct ThreadPool;
+#[derive(Debug)]
+pub struct PoolCreationError;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        ThreadPool
+    }
+
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size > 0 {
+            Ok(ThreadPool::new(size))
+        } else {
+            Err(PoolCreationError)
+        }
+    }
+    
+    pub fn execute<F>(&self, f: F)
+        where
+            F: FnOnce() + Send + 'static,
+        {
+    }
+}
